713fca0af012b5e42b34adcb75269c2779449091
Marwai89 <114883663+Marwai89@users.noreply.github.com>
2023-03-12 20:54:53 -0400

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,25 @@
+package Breakfastrestaurantfx;
+ public class Food  {
+private String foodName;
+ private float Price;
+
+
+public String getFoodName() {
+return foodName;
+}
+
+public void setFoodName(String foodName) {
+this.foodName = foodName;
+}
+
+public float getPrice() {
+       
+return Price;
+}
+
+public void setPrice(float Price) {
+// this.price = Price;
+}
+     
+ }
+}
